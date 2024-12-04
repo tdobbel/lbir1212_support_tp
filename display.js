8f246e8ec78bac7c61618443fe965ef2485cfaf5
Dobby <thomas.dobbelaere@uclouvain.be>
2024-12-04 13:08:35 +0100

    add invertColor and change keybinding

--- display.js
+++ display.js
@@ -23,8 +23,22 @@ function toggleAll() {
     }
 }
 
+function invertColors() {
+    let body = document.body;
+    to_dark = body.style.backgroundColor === "white";
+    body.style.backgroundColor = to_dark ? "black" : "white";
+    body.style.color = to_dark ? "white" : "black";
+    let imgs = document.getElementsByTagName("img");
+    for (let img of imgs) {
+        img.style.filter = to_dark ? "invert(0)" : "invert(1)";
+    }
+}
+
 window.onkeydown = function (e) {
-    if (e.keyCode == 17) {
+    if (e.key === "t") {
         toggleAll();
+    }
+    else if (e.key === "i") {
+        invertColors();
     }
 }
